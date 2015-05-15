1cac708acea8c576ea852aaee25790d48898955a
Chris Parkin <chris.d.parkin@gmail.com>
2015-05-14 23:42:18 -0400

    project_copy_request model, minor changes to data request form and incoming/outgoing requests

--- app/models/project_copy_request.rb
+++ app/models/project_copy_request.rb
@@ -1,4 +1,16 @@
 class ProjectCopyRequest < ActiveRecord::Base
   belongs_to :user
   validates :user_id, :project_id, presence: true
+
+  def to_s
+    if self.include_data == true 
+      return "Extraction Forms Only"
+    elsif self.include_studies == true 
+      return "Extraction Forms & Study List"
+    elsif self.include_forms == true
+      return "Extraction Forms, Studies & Extracted Data"
+    else
+      return "--"
+    end
+  end
 end
