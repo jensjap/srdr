a07b72b520cb73118dcd5fbc169ca48e4e65d02c
Jens Jap <jens_jap@brown.edu>
2015-10-25 16:34:58 -0400

    Change datatypes in SRDR Quality Improvement Questionnaire.
    MySQL is having trouble with very large data rows. Since we
    used exclusively STRING for the questionnaire it breaks when
    running migrate. STRING translates to varchar(255) in MySQL.
    One solution is to change the data type to BLOB or TEXT.
    
    Please see
    https://www.percona.com/blog/2011/04/07/innodb-row-size-limitation/
    
    for detailed discussion.

--- db/migrate/20151015195809_add_q1_can_followup_to_srdr_quality_improvement_questionnaire.rb
+++ db/migrate/20151015195809_add_q1_can_followup_to_srdr_quality_improvement_questionnaire.rb
@@ -1,6 +1,6 @@
 class AddQ1CanFollowupToSrdrQualityImprovementQuestionnaire < ActiveRecord::Migration
   def self.up
-    add_column :srdr_quality_improvement_questionnaires, :q1_can_followup, :string
+    add_column :srdr_quality_improvement_questionnaires, :q1_can_followup, :text, after: :q1_email
   end
 
   def self.down
